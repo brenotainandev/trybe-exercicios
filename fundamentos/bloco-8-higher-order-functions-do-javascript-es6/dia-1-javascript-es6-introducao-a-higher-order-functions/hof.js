8cb6da77931e759232efe3e112f1ca12b097e4d0
Breno Tainan <brenotaguiar@gmail.com>
2021-10-04 19:19:35 -0300

    requisito 01

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/hof.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/hof.js
@@ -1,8 +1,16 @@
-const newEmployees = () => {
+const newEmployees = (geradorEmail) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: geradorEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: geradorEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: geradorEmail('Luiza Drumond'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  const geradorEmail = (nomeCompleto) => {
+    const nome = nomeCompleto.toLowerCase().split(' ').join('_');
+    const email = `${nome}@trybe.com`;
+    return {nomeCompleto, email: email};
+  }
+
+  console.log(newEmployees(geradorEmail));
